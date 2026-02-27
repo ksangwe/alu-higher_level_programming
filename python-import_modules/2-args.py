78a40bde62f95dea946d226bab955747b1cdcc9b
Sangwe Eddy Kelly <k.sangwe@alustudent.com>
2026-02-27 15:46:52 -0800

    Task 2: Make script dynamic with argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, "" if argc == 1 else "s"))
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
