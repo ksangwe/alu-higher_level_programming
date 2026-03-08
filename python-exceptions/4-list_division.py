d5691f571d61a5e8e93d6d29dd1e72a5233c1d62
Sangwe Eddy Kelly <k.sangwe@alustudent.com>
2026-03-08 13:33:18 -0700

    Fix PEP8 in list_division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
+    new_list = []
+
     for i in range(list_length):
-        div = 0
         try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-                raise TypeError
-            div = a / b
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
-        except IndexError:
-            print("out of range")
-            div = 0
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            div = 0
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
         finally:
-            result.append(div)
-    return result
+            new_list.append(result)
+
+    return new_list
