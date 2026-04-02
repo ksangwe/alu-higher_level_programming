8c82ae9e16f83c33fffa82e24be5f88fdbe8bbeb
ksangwe <k.sangwe@alustudent.com>
2026-04-02 18:50:03 +0000

    Task 4: fetch status using requests

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches URL using requests and displays status"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
