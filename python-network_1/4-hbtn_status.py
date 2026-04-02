3480255ce8c59fcd164f09fc02ab9715e294137c
ksangwe <k.sangwe@alustudent.com>
2026-04-02 19:03:50 +0000

    Fix Task 4 final

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Fetch URL using requests and display response safely"""
+"""Fetch URL using requests and display response"""
 
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = sys.argv[1]
 
     try:
         response = requests.get(url)
+        content = response.text
+
         print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
 
     except requests.exceptions.RequestException:
         print("Body response:")
         print("\t- type: <class 'str'>")
-        print("\t- content: OK")
+        print("\t- content: Error")
