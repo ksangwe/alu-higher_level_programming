da9a8ebf5a449101aa04a8e1be6b2caec8583d30
ksangwe <k.sangwe@alustudent.com>
2026-04-02 13:23:32 +0000

    Task 0: uppercase keywords and correct privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,15 @@
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
 
--- List privileges for user_0d_2
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+CREATE DATABASE IF NOT EXISTS user_2_db;
+
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
