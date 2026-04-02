cef9a1c619e5601eeceebc7920e67302c76c1012
ksangwe <k.sangwe@alustudent.com>
2026-04-02 19:18:06 +0000

    Task 5 response header value

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Gets X-Request-Id from response header"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    print(response.headers.get("X-Request-Id"))
