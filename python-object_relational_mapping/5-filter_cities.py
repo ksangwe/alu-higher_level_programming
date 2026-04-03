925838c7031a30dec7cb443bf5fdf0406234e90d
ksangwe <k.sangwe@alustudent.com>
2026-04-03 08:58:31 +0000

    Task 5: Filter cities by state safely, PEP8 fixed

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
 """
-This script lists all cities of a given state from the database hbtn_0e_4_usa.
-It takes 4 arguments: mysql username, mysql password, database name, state name.
-It is safe from SQL injection.
+Lists all cities of a given state from the database hbtn_0e_4_usa.
+This script is safe from SQL injection and uses only one execute() call.
+Usage: ./5-filter_cities.py <mysql username> <mysql password> \
+<database name> <state name>
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command line arguments
+    # Check for the correct number of arguments
     if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <username> <password> <database> <state_name>")
+        print("Usage: ./5-filter_cities.py <user> <passwd> <db> <state>")
         sys.exit(1)
 
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    user, passwd, db_name, state_name = sys.argv[1], sys.argv[2], \
+                                       sys.argv[3], sys.argv[4]
 
-    # Connect to MySQL
+    # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name, charset="utf8")
+                         user=user, passwd=passwd, db=db_name,
+                         charset="utf8")
     cursor = db.cursor()
 
-    # SQL query: join cities with states, filter by state name (safe using placeholders)
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
+    # SQL query safe from injection
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
 
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
 
-    # Print city names comma-separated
+    # Print city names separated by commas
     print(", ".join([row[0] for row in rows]))
 
     cursor.close()
