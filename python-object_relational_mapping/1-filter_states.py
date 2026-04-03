e6d7bf900fd2512a3b20fc85a75e9b1ec4c23709
ksangwe <k.sangwe@alustudent.com>
2026-04-03 08:10:31 +0000

    Task 1: Filter states starting with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lists all states starting with N from the database
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Connects to MySQL and filters states"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+
+    # 🔥 ONLY CHANGE FROM TASK 0
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
